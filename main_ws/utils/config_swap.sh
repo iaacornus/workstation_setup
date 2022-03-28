4aed11a23b9230cbba4481fb791f04712d1779c0
iaacornus <96870156+iaacornus@users.noreply.github.com>
2022-03-28 14:23:18 +0800

    for swapping ssh config files
    Former-commit-id: b648585dec5c27a76cea5ea5a29997ae51cc8ec2

---
+++ main_ws/utils/config_swap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ ! -n $1 ]]
+then
+	echo -e "\e[1;31m> No input given.\e[0m"
+else
+	read -p "Swap config for $1 [y/N] ? " ans
+	if [[ $ans == *"N"* ]] || [[ $ans == *"n"* ]]
+	then
+		echo -e "\e[1;31m> User aborted. Exiting.\e[0m"
+		exit 1
+	else
+		if [ -d "$HOME/.ssh/config" ]
+		then
+			rm $HOME/.ssh/config
+		fi
+
+		cp $HOME/.ssh/$1 $HOME/.ssh/config	
+		echo -e "\e[1;32m> Config for $1 swapped.\e[0m"
+	fi
+fi
+
