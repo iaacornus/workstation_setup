9bd8deb561ebf7807c0c10b9e0e19a63325f482b
iaacornus <iaacornus@gmail.com>
2022-04-26 00:58:42 +0800

    removed clear swap function
    Former-commit-id: e0bea43d5a7b9f17cf4a424a2f0b346815395b28 [formerly fc57a7fbfaa7ad4d5e72529a61d14353d4bfc98d]
    Former-commit-id: cc0008a6920baf7a49b919a970ecbf06d883d5a1

--- main_ws/utils/utils.sh
+++ main_ws/utils/utils.sh
@@ -39,33 +39,6 @@ then
 
 	fi
 
-elif [[ $1 == *"clear-swap"* ]]
-then
-	#* passed
-	free -h | grep "Swap"
-	read -p  "Proceed in cleaning swap [y/N]? " ans
-
-	if [[ $ans == *"y"* ]]
-	then
-		echo -e "\e[1;32m> Cleaning swap as issued by: \e[0m$USER@$HOSTNAME"
-		sudo swapoff -a
-
-		# sleep for 30s after turning off swap
-		sec=30
-		while [ $sec -ge 0 ]; do
-			echo -ne "\e[1;32m> /dev/swap deactivated, reactivating after:\e[0m" $sec"s\033[0K\r"
-			let "sec=sec-1"
-			sleep 1
-		done
-
-		sudo swapon -a
-		echo -ne "\n\r"
-		echo -e "\e[1;32m> /dev/swap reactivated, successfully\e[0m"
-		free -h | grep "Swap"
-	else
-		echo -e "\e[1;31m> Procedure aborted by: \e[31m$USER\e[0m"
-	fi
-
 elif [[ $1 == *"tex-gen"* ]]
 then
 	#* passed
