9f2b733d342c495eb05c81e162eac61ac8bcf87c
iaacornus <iaacornus.devel@gmail.com>
2022-07-10 22:34:51 +0800

    only display colorscripts if me the conditions
    Former-commit-id: 3f8c73952e611028a7499338a86c0ce5b30d0bb2 [formerly fd6dbad0176d93c8ad138f25dd918240d6308113]
    Former-commit-id: e9a735bd20a5ce2e2ed73fb972c1653b94da7cc0

--- main_ws/utils/clear.sh
+++ main_ws/utils/clear.sh
@@ -1,5 +1,7 @@
 clear
-ls $HOME/.sys/toys | sort -R | tail -$N | while read file; do
-	$HOME/.sys/toys/./$file
-	break
-done
+if [[ $(tput cols) -gt 79 && $(tput lines) -gt 29 ]]; then
+    ls $HOME/.sys/toys | sort -R | tail -$N | while read file; do
+	    $HOME/.sys/toys/./$file
+	    break
+    done
+fi
