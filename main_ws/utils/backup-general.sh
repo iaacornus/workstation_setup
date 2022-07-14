d248f7b84865b35e79e055ccea8cafbf182c5d5d
iaacornus <iaacornus.devel@gmail.com>
2022-07-15 00:40:31 +0800

    separated the two scripts
    Former-commit-id: 33e7581d96a8bcf167beede0d9c5648d638020ce [formerly 2352d43d54bbaea2eb149939f1d93fa98d961e01]
    Former-commit-id: 32432bfee116f4a5e89cd50360fba6b1043c2c9d

--- main_ws/utils/backup-general.sh
+++ main_ws/utils/backup-general.sh
@@ -81,10 +81,3 @@ gpg --export-ownertrust > $HOME/Storage/backup/gpg/trustdb
 
 echo -e "\033[1;32m[+] Successfully exported the GPG keys\033[0m"
 
-echo -e "\033[1m[>] Executing full system upgrade\033[0m"
-echo -e ">>> Updating base system image via: \033[36mrpm-ostree upgrade\033[0m"
-rpm-ostree upgrade
-echo -e ">>> Updating user applications via: \033[36mflatpak update\033[0m"
-flatpak update
-
-echo -e "\033[1;32m[+] Successfully upgraded the system\033[0m"
