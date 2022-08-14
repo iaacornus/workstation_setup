050db2629a7919db163e789fa363ebbf87fb621d
iaacornus <iaacornus.devel@gmail.com>
2022-08-14 16:10:03 +0800

    update wallpapers
    Former-commit-id: e95adb23e62948a1460c78b25d558fe50bb99c65 [formerly af28bca56015963a2497e0e5731140f452ee8843]
    Former-commit-id: 91084ca396e7a98f2c7db2c7029e9ec1c1ae9b3c

--- main_ws/utils/change-wallpaper.sh
+++ main_ws/utils/change-wallpaper.sh
@@ -3,11 +3,11 @@
 current_time=$(date +%H:%M)
 
 if [[ "$current_time" > "06:00" || "$current_time" == "06:00"  ]] && [[ "$current_time" < "14:00" ]]; then
-    wallpaper="am_$(shuf -i1-3 -n1).jpg"
+    wallpaper="am_$(shuf -i1-8 -n1).jpg"
 elif [[ "$current_time" > "14:00" || "$current_time" == "14:00" ]] && [[ "$current_time" < "20:00" ]]; then
-    wallpaper="nn_$(shuf -i1-2 -n1).jpg"
+    wallpaper="pm_$(shuf -i1-16 -n1).jpg"
 elif [[ "$current_time" > "20:00" || "$current_time" == "20:00" ]] || [[ "$current_time" < "06:00" ]]; then
-    wallpaper="pm_$(shuf -i1-2 -n1).jpg"
+    wallpaper="gn_$(shuf -i1-15 -n1).jpg"
 fi
 
 gsettings set org.gnome.desktop.background picture-uri file:///$HOME/Pictures/wallpaper/$wallpaper
