eef85e6250f6d666dd0e31ec299b1cd26642210e
iaacornus <iaacornus.devel@gmail.com>
2022-08-14 16:26:56 +0800

    update symbols
    Former-commit-id: 2963004044f6acd41d1140cf0e1b26ef0e0a69f5 [formerly a9f25399d142513cc744a4307001b057bb8318a7]
    Former-commit-id: 87fb3ac8898dde10eaf293dd1e22ff1d8c792d60

--- main_ws/utils/utils.sh
+++ main_ws/utils/utils.sh
@@ -2,27 +2,27 @@
 
 if [[ $1 == *"sys-info"* ]]; then
 	if [[ $2 == *"less"* ]]; then
-		echo -e ">>> Memory and Swap usage:"
+		echo -e "[=] Memory and Swap usage:"
 		free -h
-		echo -e ">>> CPU usage:\e[36m $(top -n 1 | grep -E 'Cpu|Tasks')\e[0m"
-		echo -e ">>> Number of running services from systemctl:\e[36m $(systemctl list-units --type=service --state=running | grep listed)\e[0m"
-		echo -e ">>> Number of failed services from systemd:\e[36m $(systemctl --failed | grep listed)\e[0m"
-		echo -e ">>> Uptime: \e[36m$(uptime)\e[0m"
+		echo -e "[=] CPU usage:\e[36m $(top -n 1 | grep -E 'Cpu|Tasks')\e[0m"
+		echo -e "[=] Number of running services from systemctl:\e[36m $(systemctl list-units --type=service --state=running | grep listed)\e[0m"
+		echo -e "[=] Number of failed services from systemd:\e[36m $(systemctl --failed | grep listed)\e[0m"
+		echo -e "[=] Uptime: \e[36m$(uptime)\e[0m"
 	else
-		echo -e ">>> Memory and Swap usage:"
+		echo -e "[=] Memory and Swap usage:"
 		free -h
-		echo -e ">>> CPU usage:"
+		echo -e "[=] CPU usage:"
         top -n 1 | grep -E 'Cpu|Tasks'
-		echo -e ">>> Storage usage of \e[36m/dev/sdb\e[0m:"
+		echo -e "[=] Storage usage of \e[36m/dev/sdb\e[0m:"
         df -H | grep 'sdb'
-		echo -e ">>> Storage usage of \e[36m/var/home\e[0m (\e[36m$USER\e[0m):"
+		echo -e "[=] Storage usage of \e[36m/var/home\e[0m (\e[36m$USER\e[0m):"
         df -H $HOME | grep 'dev'
-		echo -e ">>> Storage used of \e[36m$HOME/Storage\e[0m:"
+		echo -e "[=] Storage used of \e[36m$HOME/Storage\e[0m:"
         df -H /dev/sda1
-		echo -e ">>> Number of running services from systemctl: \e[36m$(systemctl list-units --type=service --state=running | grep 'listed')\e[0m"
-		echo -e ">>> Number of failed services from systemd: \e[36m$(systemctl --failed | grep 'listed')\e[0m"
-		echo -e ">>> Uptime: \e[36m$(uptime)\e[0m"
-		echo -e ">>> System performance time: \e[36m$(systemd-analyze)\e[0m"
+		echo -e "[=] Number of running services from systemctl: \e[36m$(systemctl list-units --type=service --state=running | grep 'listed')\e[0m"
+		echo -e "[=] Number of failed services from systemd: \e[36m$(systemctl --failed | grep 'listed')\e[0m"
+		echo -e "[=] Uptime: \e[36m$(uptime)\e[0m"
+		echo -e "[=] System performance time: \e[36m$(systemd-analyze)\e[0m"
 	fi
 elif [[ $1 == *"tex-gen"* ]]; then
 	#* passed
@@ -36,7 +36,7 @@ elif [[ $1 == *"tex-gen"* ]]; then
 	echo -e "\e[1;32m[+] Template created in:\e[0m\e[1m $PWD/$2.tex\e[0m"
 elif [[ $1 == *"clean-up"* ]]; then
 	echo -e "\e[1m[>] Cleaning up the system.\e[0m"
-	echo -e ">>> Removing journal files with size of:\e[36m$(journalctl --disk-usage)\e[0m"
+	echo -e ">>> Removing journal files with size of: \e[36m$(journalctl --disk-usage)\e[0m"
 
 	sudo journalctl --vacuum-time=3d
 
