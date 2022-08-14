4c22fc7314971925248e01f54e524b5489bb6b80
iaacornus <iaacornus.devel@gmail.com>
2022-08-14 16:24:06 +0800

    sorted wallpaper
    Former-commit-id: b43bcf84a9a0c46f1b7a18bccc1d7e2ffd6baa2f [formerly 060fa1197eddafb53068c9955913229c60062330]
    Former-commit-id: e7e2dbcef54fdae72c58549de109159f4caa3353

--- main_ws/utils/change-wallpaper.sh
+++ main_ws/utils/change-wallpaper.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 current_time=$(date +%H:%M)
+guess=$(shuf -i1-2 -n1)
 
-if [[ "$current_time" > "06:00" || "$current_time" == "06:00"  ]] && [[ "$current_time" < "12:00" ]]; then
-    wallpaper="am_$(shuf -i1-8 -n1).jpg"
-elif [[ "$current_time" > "12:00" || "$current_time" == "12:00" ]] && [[ "$current_time" < "19:00" ]]; then
-    wallpaper="pm_$(shuf -i1-16 -n1).jpg"
-elif [[ "$current_time" > "19:00" || "$current_time" == "19:00" ]] || [[ "$current_time" < "06:00" ]]; then
-    wallpaper="gn_$(shuf -i1-15 -n1).jpg"
+if [[ $guess -eq 2 ]]; then
+    if [[ "$current_time" > "06:00" || "$current_time" == "06:00"  ]] && [[ "$current_time" < "12:00" ]]; then
+        wallpaper="am_$(shuf -i1-8 -n1).jpg"
+    elif [[ "$current_time" > "12:00" || "$current_time" == "12:00" ]] && [[ "$current_time" < "19:00" ]]; then
+        wallpaper="pm_$(shuf -i1-8 -n1).jpg"
+    elif [[ "$current_time" > "19:00" || "$current_time" == "19:00" ]] || [[ "$current_time" < "06:00" ]]; then
+        wallpaper="gn_$(shuf -i1-8 -n1).jpg"
+    fi
+else
+    wallpaper="main_$(shuf -i1-8 -n1).jpg"
 fi
 
 gsettings set org.gnome.desktop.background picture-uri file:///$HOME/Pictures/wallpaper/$wallpaper
