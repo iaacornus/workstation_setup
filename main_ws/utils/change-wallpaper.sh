5338c3230ecb906eb61be3234277fe58a8106639
iaacornus <iaacornus.devel@gmail.com>
2022-08-27 23:54:56 +0800

    fix wallpaper setup script
    Former-commit-id: e9f4c2f09e891bd25ecb71e99e6ed95c497195a2 [formerly b07dd9712f6292397b09e20e65c9b67922a7d3bc]
    Former-commit-id: f003b94d43af7204231a817e4e8b9f8fce5c9460

--- main_ws/utils/change-wallpaper.sh
+++ main_ws/utils/change-wallpaper.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
 current_time=$(date +%H:%M)
-guess=$(shuf -i1-2 -n1)
 
-if [[ $guess -eq 2 ]]; then
-    if [[ "$current_time" > "06:00" || "$current_time" == "06:00"  ]] && [[ "$current_time" < "12:00" ]]; then
-        wallpaper="am_$(shuf -i1-8 -n1).jpg"
-    elif [[ "$current_time" > "12:00" || "$current_time" == "12:00" ]] && [[ "$current_time" < "19:00" ]]; then
-        wallpaper="pm_$(shuf -i1-8 -n1).jpg"
-    elif [[ "$current_time" > "19:00" || "$current_time" == "19:00" ]] || [[ "$current_time" < "06:00" ]]; then
-        wallpaper="gn_$(shuf -i1-8 -n1).jpg"
-    fi
-else
-    wallpaper="main_$(shuf -i1-8 -n1).jpg"
+if [[ "$current_time" > "06:00" || "$current_time" == "06:00"  ]] && [[ "$current_time" < "12:00" ]]; then
+    wallpaper="blobs-l.svg"
+elif [[ "$current_time" > "19:00" || "$current_time" == "19:00" ]] || [[ "$current_time" < "06:00" ]]; then
+    wallpaper="blobs-d.svg"
 fi
 
-gsettings set org.gnome.desktop.background picture-uri file:///$HOME/Pictures/wallpaper/$wallpaper
-gsettings set org.gnome.desktop.screensaver picture-uri file:///$HOME/Pictures/wallpaper/$wallpaper
+gsettings set org.gnome.desktop.background picture-uri file:///$HOME/Pictures/Wallpapers/$wallpaper
+gsettings set org.gnome.desktop.screensaver picture-uri file:///$HOME/Pictures/Wallpapers/$wallpaper
