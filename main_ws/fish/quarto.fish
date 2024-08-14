aeef709921b7e05090e417ff3f92eeee48a06495
James Aaron Erang <iaacornus.devel@gmail.com>
2024-08-14 13:58:49 +0800

    remove quarto

--- main_ws/fish/quarto.fish
+++
@@ -1,4 +0,0 @@
-function quarto --wraps='toolbox -c fedora-toolbox-40 run /var/home/iaacornus/.local/bin/quarto' --description 'alias quarto=toolbox -c fedora-toolbox-40 run /var/home/iaacornus/.local/bin/quarto'
-  toolbox -c fedora-toolbox-40 run /var/home/iaacornus/.local/bin/quarto $argv
-        
-end
