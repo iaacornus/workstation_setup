36d4344977d2233bf38ec294dd1d443deb5a218c
James Aaron Erang <iaacornus.devel@gmail.com>
2024-08-19 19:38:55 +0800

    update fish config

--- main_ws/functions/ffthemeupdate.fish
+++
@@ -1,4 +0,0 @@
-function ffthemeupdate --wraps='curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash' --description 'alias ffthemeupdate=curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash'
-  curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash $argv
-        
-end
