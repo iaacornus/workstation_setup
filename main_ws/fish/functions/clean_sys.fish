29259121e86c9944f924aa8a0bc4d2470f8d3497
James Aaron Erang <iaacornus.devel@gmail.com>
2024-08-24 14:47:52 +0800

    update functions

--- main_ws/fish/functions/clean_sys.fish
+++ main_ws/fish/functions/clean_sys.fish
@@ -1,4 +1,4 @@
-function clean_sys --wraps='sudo ostree admin cleanup -vv && rpm-ostree cleanup -bpmr && toolbox -c fedora-toolbox-40 run sudo dnf clean all' --description 'alias clean_sys=sudo ostree admin cleanup -vv && rpm-ostree cleanup -bpmr && toolbox -c fedora-toolbox-40 run sudo dnf clean all'
-  sudo ostree admin cleanup -vv && rpm-ostree cleanup -bpmr && toolbox -c fedora-toolbox-40 run sudo dnf clean all $argv
+function clean_sys --wraps='sudo ostree admin cleanup -vv && rpm-ostree cleanup -bpmr && toolbox -c fedora-toolbox-40 run sudo dnf clean all && conda clean -a -y' --description 'alias clean_sys=sudo ostree admin cleanup -vv && rpm-ostree cleanup -bpmr && toolbox -c fedora-toolbox-40 run sudo dnf clean all && conda clean -a -y'
+  sudo ostree admin cleanup -vv && rpm-ostree cleanup -bpmr && toolbox -c fedora-toolbox-40 run sudo dnf clean all && conda clean -a -y $argv
         
 end
