fc140671b8b18de5ccd95063af9a53dbd1f0e0ef
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-11 00:14:10 +0800

    experiment alias

---
+++ main_ws/fish/functions/experiment.fish
@@ -0,0 +1,4 @@
+function experiment --wraps='toolbox enter fedora-toolbox-39' --description 'alias experiment=toolbox enter fedora-toolbox-39'
+  toolbox enter fedora-toolbox-39 $argv
+        
+end
