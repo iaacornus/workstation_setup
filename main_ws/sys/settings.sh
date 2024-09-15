8550bc75c9f1282ee1a2d4ff45b5c1155f5916eb
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-15 09:21:41 +0800

    include rstudio config

--- main_ws/sys/settings.sh
+++ main_ws/sys/settings.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+cp -r $HOME/.local/share/config/rstudio/keybindings/* $HOME/.config/rstudio/keybindings
+cp $HOME/.local/share/config/rstudio/rstudio-prefs.json $HOME/.config/rstudio/rstudio-prefs.json
 cp -r $HOME/.local/share/config/code/* $HOME/.config/Code/User/
