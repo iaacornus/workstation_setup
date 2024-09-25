6c2ee78d0549a0dc7bb741be260f4acee4be3bfc
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-25 11:05:44 +0800

    remove batshit

--- main_ws/config/jupyter/jupyterlab-desktop/jlab_server/shell/condabin/conda-hook.ps1
+++
@@ -1,9 +0,0 @@
-$Env:CONDA_EXE = "/var/home/iaacornus/.var/app/org.jupyter.JupyterLab/config/jupyterlab-desktop/jlab_server/bin/conda"
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
-$Env:_CONDA_ROOT = "/var/home/iaacornus/.var/app/org.jupyter.JupyterLab/config/jupyterlab-desktop/jlab_server"
-$Env:_CONDA_EXE = "/var/home/iaacornus/.var/app/org.jupyter.JupyterLab/config/jupyterlab-desktop/jlab_server/bin/conda"
-$CondaModuleArgs = @{ChangePs1 = $True}
-Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
-
-Remove-Variable CondaModuleArgs
