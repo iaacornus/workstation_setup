ad379911a22773ab08786bf0a09909fbf3bcf0fc
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-27 10:08:36 +0800

    new alias

---
+++ main_ws/fish/functions/asciiquarium.fish
@@ -0,0 +1,4 @@
+function asciiquarium --description 'alias asciiquarium=toolbox -c fedora-toolbox-40 run asciiquarium'
+  toolbox -c fedora-toolbox-40 run asciiquarium $argv
+        
+end
---
+++ main_ws/fish/functions/cmatrix.fish
@@ -0,0 +1,4 @@
+function cmatrix --description 'alias cmatrix=toolbox -c fedora-toolbox-40 run cmatrix'
+  toolbox -c fedora-toolbox-40 run cmatrix $argv
+        
+end
