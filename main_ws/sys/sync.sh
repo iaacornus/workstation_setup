6dbd5f807be471f3b9709ef6d65d7500a137c113
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-03 00:17:20 +0800

    sync script

---
+++ main_ws/sys/sync.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cp $HOME/.config/mimeapps.list $HOME/Code/workstation_setup/main_ws/config/ &&\
+cp -r $HOME/Templates/* $HOME/Code/workstation_setup/main_ws/Templates/ &&\
+cp -r $HOME/.config/fish/* $HOME/Code/workstation_setup/main_ws/fish/ &&\
+cp $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/settings.json -t $HOME/.local/share/config/code/ &&\
+cp $HOME/.local/share/config/code/* $HOME/Code/workstation_setup/main_ws/config/code/ &&\
+cp -r $HOME/.local/share/icons/* $HOME/Code/workstation_setup/main_ws/icons/ &&\
+cp -r $HOME/.local/share/applications/* $HOME/Code/workstation_setup/main_ws/applications/ &&\
+cp -r $HOME/.config/Code/User/profiles/ $HOME/Code/workstation_setup/main_ws/config/code/ &&\
+cp $HOME/.config/rstudio/rstudio-prefs.json $HOME/.local/share/config/rstudio/rstudio-prefs.json &&\
+cp -r $HOME/.config/rstudio/keybindings/* $HOME/.local/share/config/rstudio/keybindings/ &&\
+cp -r $HOME/.local/share/config/* $HOME/Code/workstation_setup/main_ws/lconfig/ &&\
+cp -r $HOME/.mozilla/firefox/y6r42lsy.default-release/prefs.js $HOME/Code/workstation_setup/main_ws/config &&\
+cp -r $HOME/.mozilla/firefox/y6r42lsy.default-release/user.js $HOME/Code/workstation_setup/main_ws/config
+
