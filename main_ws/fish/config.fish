04365331f4f67e22caa8ab7d9647a043f374a84a
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-03 01:03:40 +0800

    remove conda bullshit causing slow

--- main_ws/fish/config.fish
+++ main_ws/fish/config.fish
@@ -4,13 +4,5 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /var/home/iaacornus/.anaconda3/bin/conda
-    eval /var/home/iaacornus/.anaconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/var/home/iaacornus/.anaconda3/etc/fish/conf.d/conda.fish"
-        . "/var/home/iaacornus/.anaconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/var/home/iaacornus/.anaconda3/bin" $PATH
-    end
-end
+set -x PATH "/var/home/iaacornus/.anaconda3/bin" $PATH
 # <<< conda initialize <<<
