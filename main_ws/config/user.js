b6a0e6b35cbbf1c796e1e93fcb0d419c3effa008
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-03 00:24:48 +0800

    ff user.js

---
+++ main_ws/config/user.js
@@ -0,0 +1,19 @@
+/* user.js
+ * https://github.com/rafaelmardojai/firefox-gnome-theme/
+ */
+
+// Enable customChrome.css
+
+// Set UI density to normal
+
+// Enable SVG context-propertes
+
+// Disable private window dark theme
+
+// Enable rounded bottom window corners
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.uidensity", 0);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.theme.dark-private-windows", false);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
