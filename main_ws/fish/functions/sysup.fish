20f4cea7251297b81b245c36800af7e9d5c4968a
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-05 01:48:54 +0800

    remove update_all and include systemctl reboot in hwup, sysup and utilsup

--- main_ws/fish/functions/sysup.fish
+++ main_ws/fish/functions/sysup.fish
@@ -1,4 +1,4 @@
-function sysup --wraps='echo 7thupholder | sudo -S tuned-adm profile throughput-performance && rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y' --description 'alias sysup=echo 7thupholder | sudo -S tuned-adm profile throughput-performance && rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y'
-  echo 7thupholder | sudo -S tuned-adm profile throughput-performance && rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y $argv
+function sysup --wraps='echo 7thupholder | sudo -S tuned-adm profile throughput-performance && rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y' --wraps='rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y' --wraps='rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y; systemctl reboot' --description 'alias sysup=rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y; systemctl reboot'
+  rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y; systemctl reboot $argv
         
 end
--- main_ws/fish/functions/update_all.fish
+++
@@ -1,4 +0,0 @@
-function update_all --wraps='echo 7thupholder | sudo -S tuned-adm profile throughput-performance && sysup && utilsup && systemctl reboot' --description 'alias update_all=echo 7thupholder | sudo -S tuned-adm profile throughput-performance && sysup && utilsup && systemctl reboot'
-  echo 7thupholder | sudo -S tuned-adm profile throughput-performance && sysup && utilsup && systemctl reboot $argv
-        
-end
