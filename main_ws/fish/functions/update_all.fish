e3ef19b618d96cdd554db4519549ddf9c9f2668d
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-04 23:01:32 +0800

    remove update_all

--- main_ws/fish/functions/update_all.fish
+++
@@ -1,4 +0,0 @@
-function update_all --wraps='hwup && sysup && utilsup && systemctl reboot' --description 'alias update_all=hwup && sysup && utilsup && systemctl reboot'
-  hwup && sysup && utilsup && systemctl reboot $argv
-        
-end
