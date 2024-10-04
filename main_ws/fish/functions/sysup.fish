3b86c05af714e9e5679d5f92691372537db1296e
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-04 22:17:13 +0800

    -S

--- main_ws/fish/functions/sysup.fish
+++ main_ws/fish/functions/sysup.fish
@@ -1,4 +1,4 @@
-function sysup --wraps='rpm-ostree upgrade && echo 7thupholder | sudo flatpak update --system -y' --description 'alias sysup=rpm-ostree upgrade && echo 7thupholder | sudo flatpak update --system -y'
-  rpm-ostree upgrade && echo 7thupholder | sudo flatpak update --system -y $argv
+function sysup --wraps='rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y' --description 'alias sysup=rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y'
+  rpm-ostree upgrade && echo 7thupholder | sudo -S flatpak update --system -y $argv
         
 end
