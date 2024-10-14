f06f1beacc90020d04591896bc4ec5d974d7aeab
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-15 02:10:41 +0800

    ranger alias

---
+++ main_ws/fish/functions/fm.fish
@@ -0,0 +1,4 @@
+function fm --wraps='toolbox -c fedora-toolbox-40 run ranger' --description 'alias fm=toolbox -c fedora-toolbox-40 run ranger'
+  toolbox -c fedora-toolbox-40 run ranger $argv
+        
+end
