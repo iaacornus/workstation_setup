3c4beb2a57af5a50d3b85db2b60b13e549767679
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-20 00:24:28 +0800

    comment some stuff

--- main_ws/sys/sync.sh
+++ main_ws/sys/sync.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-cp $HOME/.config/mimeapps.list $HOME/Code/workstation_setup/main_ws/config/ &&\
-cp -r $HOME/Templates/* $HOME/Code/workstation_setup/main_ws/Templates/ &&\
-cp -r $HOME/.config/fish/* $HOME/Code/workstation_setup/main_ws/fish/ &&\
-cp $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/settings.json -t $HOME/.local/share/config/code/ &&\
-cp $HOME/.local/share/config/code/* $HOME/Code/workstation_setup/main_ws/config/code/ &&\
-cp -r $HOME/.local/share/icons/* $HOME/Code/workstation_setup/main_ws/icons/ &&\
-cp -r $HOME/.local/share/applications/* $HOME/Code/workstation_setup/main_ws/applications/ &&\
-cp -r $HOME/.config/Code/User/profiles/* $HOME/Code/workstation_setup/main_ws/config/code/ &&\
-cp -r $HOME/.config/rstudio/* $HOME/.local/share/config/rstudio/ &&\
-cp -r $HOME/.local/share/config/* $HOME/Code/workstation_setup/main_ws/lconfig/ &&\
-cp -r $HOME/.mozilla/firefox/3uvsc697.default-release/user.js* $HOME/Code/workstation_setup/main_ws/config/firefox &&\
-cp $HOME/.config/starship.toml $HOME/Code/workstation_setup/main_ws/config/starship.toml &&\
-cp $HOME/.local/bin/*.sh $HOME/Code/workstation_setup/main_ws/sys/ &&\
-cp -r $HOME/.local/share/applications/* $HOME/Code/workstation_setup/main_ws/applications/ &&\
-cp $HOME/.var/app/org.texstudio.TeXstudio/config/texstudio/texstudio.ini $HOME/Code/workstation_setup/main_ws/config/ &&\
-cp $HOME/.var/app/com.github.xournalpp.xournalpp/config/xournalpp/settings.xml $HOME/Code/workstation_setup/main_ws/config/xournalpp/settings.xml &&\
-cp $HOME/.var/app/com.github.xournalpp.xournalpp/config/xournalpp/palette.gpl $HOME/Code/workstation_setup/main_ws/config/xournalpp/palette.gpl &&\
-cp -r $HOME/.local/share/icons/* $HOME/Code/workstation_setup/main_ws/icons/ &&\
-cp -r $HOME/.local/share/icons/* $HOME/Code/workstation_setup/main_ws/icons/
+cp $HOME/.config/mimeapps.list $HOME/Code/workstation_setup/main_ws/config/ # mimeapps
+cp -r $HOME/Templates/* $HOME/Code/workstation_setup/main_ws/Templates/ # templates
+cp -r $HOME/.config/fish/* $HOME/Code/workstation_setup/main_ws/fish/ # fish config
+cp $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/settings.json -t $HOME/.local/share/config/code/ # code prefs -> .local/share/code
+cp -r $HOME/.config/Code/User/profiles/* $HOME/Code/workstation_setup/main_ws/config/code/ # code profiles -> .local/share/config
+cp -r $HOME/.config/rstudio/* $HOME/.local/share/config/rstudio/ # rstudio pref -> .local/share/config
+cp -r $HOME/.local/share/config/* $HOME/Code/workstation_setup/main_ws/lconfig/ # .local/share/config -> workstation_setup
+cp -r $HOME/.local/share/icons/* $HOME/Code/workstation_setup/main_ws/icons/ # icons
+cp -r $HOME/.local/share/applications/* $HOME/Code/workstation_setup/main_ws/applications/ # .desktop
+cp -r $HOME/.mozilla/firefox/3uvsc697.default-release/user.js* $HOME/Code/workstation_setup/main_ws/config/firefox  # user.js
+cp $HOME/.config/starship.toml $HOME/Code/workstation_setup/main_ws/config/starship.toml # starship
+cp $HOME/.local/bin/*.sh $HOME/Code/workstation_setup/main_ws/sys/ # scripts
+cp $HOME/.var/app/org.texstudio.TeXstudio/config/texstudio/texstudio.ini $HOME/Code/workstation_setup/main_ws/config/ # texstudio
+cp $HOME/.var/app/com.github.xournalpp.xournalpp/config/xournalpp/settings.xml $HOME/Code/workstation_setup/main_ws/config/xournalpp/settings.xml # xournal settings.xml
+cp $HOME/.var/app/com.github.xournalpp.xournalpp/config/xournalpp/palette.gpl $HOME/Code/workstation_setup/main_ws/config/xournalpp/palette.gpl # xournal palette.gpl
