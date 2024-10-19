ea5ee443d449e4fcf2ce802a06575241af919495
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-20 01:53:33 +0800

    fix nbconvert alias

--- main_ws/fish/functions/nbconvert.fish
+++ main_ws/fish/functions/nbconvert.fish
@@ -1,4 +1,4 @@
-function nbconvert --description 'alias nbconvert=toolbox -c fedora-toolbox-40 run .anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert'
-  toolbox -c fedora-toolbox-40 run .anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert $argv
+function nbconvert --wraps='toolbox -c fedora-toolbox-40 run bash -c ".anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert"' --wraps='toolbox -c fedora-toolbox-40 run bash -c "/var/home/iaacornus/.anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert"' --description 'alias nbconvert=toolbox -c fedora-toolbox-40 run /var/home/iaacornus/.anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert'
+  toolbox -c fedora-toolbox-40 run /var/home/iaacornus/.anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert $argv
         
 end
