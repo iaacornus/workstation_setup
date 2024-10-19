057446f5947bb8789f90432041c1a2971e46225d
James Aaron Erang <iaacornus.devel@gmail.com>
2024-10-19 17:43:14 +0800

    update namespaces

--- main_ws/fish/functions/nbconvert.fish
+++ main_ws/fish/functions/nbconvert.fish
@@ -1,4 +1,4 @@
-function nbconvert --description 'alias nbconvert=toolbox -c fedora-toolbox-40 run jupyter nbconvert'
-  toolbox -c fedora-toolbox-40 run jupyter nbconvert $argv
+function nbconvert --description 'alias nbconvert=toolbox -c fedora-toolbox-40 run .anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert'
+  toolbox -c fedora-toolbox-40 run .anaconda3/envs/bioinformatics_full/bin/jupyter nbconvert $argv
         
 end
