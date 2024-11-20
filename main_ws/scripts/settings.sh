902d9e7ad2d33e5eafa851fb7d6b05adef7c9607
James Aaron Erang <iaacornus.devel@gmail.com>
2024-11-20 11:26:26 +0800

    settings.sh: sync the whole rstudio config dir

--- main_ws/scripts/settings.sh
+++ main_ws/scripts/settings.sh
@@ -2,5 +2,4 @@
 
 $HOME/.local/bin/./sync.sh # sync first
 cp -r $HOME/.local/share/config/code/* $HOME/.config/Code/User/
-cp -r $HOME/.local/share/config/rstudio/keybindings/* $HOME/.config/rstudio/keybindings
-cp $HOME/.local/share/config/rstudio/rstudio-prefs.json $HOME/.config/rstudio/rstudio-prefs.json
+cp -r $HOME/.local/share/config/rstudio/* $HOME/.config/rstudio/
