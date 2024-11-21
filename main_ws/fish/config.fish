4480af75468f19b19d597927a2b5dd5fd6496688
James Aaron Erang <iaacornus.devel@gmail.com>
2024-11-21 15:29:09 +0800

    sync changes

--- main_ws/fish/config.fish
+++ main_ws/fish/config.fish
@@ -4,7 +4,6 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-. "/var/home/iaacornus/.anaconda3/etc/fish/conf.d/conda.fish"
 set -x PATH "/var/home/iaacornus/.anaconda3/bin" $PATH
 # <<< conda initialize <<<
 starship init fish | source
