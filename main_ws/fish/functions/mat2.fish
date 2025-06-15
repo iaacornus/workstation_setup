80c3d75fe36547f594a19d32e9da3aacbd2dafdf
James Aaron Erang <iaacornus.devel@gmail.com>
2025-06-15 16:45:19 +0800

    update mimeapps and mat2

--- main_ws/fish/functions/mat2.fish
+++ main_ws/fish/functions/mat2.fish
@@ -1,4 +1,4 @@
-function mat2 --description 'alias mat2=toolbox -c fedora-toolbox-39 run mat2'
-  toolbox -c fedora-toolbox-39 run mat2 $argv
+function mat2 --description 'alias mat2=toolbox -c fedora-toolbox-40 run mat2'
+  toolbox -c fedora-toolbox-40 run mat2 $argv
         
 end
