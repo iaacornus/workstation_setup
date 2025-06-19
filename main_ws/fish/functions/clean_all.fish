619f2189378b19925beddc79cdf9ab07449ef239
James Aaron Erang <iaacornus.devel@gmail.com>
2025-06-20 00:31:40 +0800

    update

--- main_ws/fish/functions/clean_all.fish
+++ main_ws/fish/functions/clean_all.fish
@@ -1,4 +1,4 @@
-function clean_all --wraps='clean_sys && clean_utils && trim && echo 7thupholder | sudo -S tuned-adm profile powersave' --wraps='clean_sys && clean_utils && echo 7thupholder | sudo -S tuned-adm profile powersave' --description 'alias clean_all=clean_sys && clean_utils && echo 7thupholder | sudo -S tuned-adm profile powersave'
-  clean_sys && clean_utils && echo 7thupholder | sudo -S tuned-adm profile powersave $argv
+function clean_all --wraps='clean_sys && clean_utils && trim && echo 7thupholder | sudo -S tuned-adm profile powersave' --wraps='clean_sys && clean_utils && echo 7thupholder | sudo -S tuned-adm profile powersave' --wraps='clean_sys && clean_utils' --description 'alias clean_all=clean_sys && clean_utils'
+  clean_sys && clean_utils $argv
         
 end
