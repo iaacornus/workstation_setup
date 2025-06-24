9abc70ea83fa8e2c3aad2850ae49480f562c8579
James Aaron Erang <iaacornus.devel@gmail.com>
2025-06-25 06:44:20 +0800

    update

--- main_ws/fish/functions/clean_utils.fish
+++ main_ws/fish/functions/clean_utils.fish
@@ -1,4 +1,4 @@
-function clean_utils --wraps='conda clean -a -y && toolbox -c fedora-toolbox-39 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-39 run sudo dnf clean all && toolbox -c fedora-toolbox-40 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-40 run sudo dnf clean all && flatpak repair' --description 'alias clean_utils=conda clean -a -y && toolbox -c fedora-toolbox-39 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-39 run sudo dnf clean all && toolbox -c fedora-toolbox-40 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-40 run sudo dnf clean all && flatpak repair'
-  conda clean -a -y && toolbox -c fedora-toolbox-39 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-39 run sudo dnf clean all && toolbox -c fedora-toolbox-40 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-40 run sudo dnf clean all && flatpak repair $argv
+function clean_utils --wraps='conda clean -a -y && toolbox -c fedora-toolbox-39 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-39 run sudo dnf clean all && toolbox -c fedora-toolbox-40 run sudo dnf autoremove -y && toolbox -c fedora-toolbox-40 run sudo dnf clean all && flatpak repair' --wraps='conda clean -a -y && flatpak repair' --description 'alias clean_utils=conda clean -a -y && flatpak repair'
+  conda clean -a -y && flatpak repair $argv
         
 end
